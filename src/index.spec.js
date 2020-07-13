5b9565d9f9dd49aeb19ed551ee12f6f7c4c01a98
Hunter Heitzman <hunter.heitzman@leankit.com>
2020-07-13 16:47:27 -0500

    Added getFormattedDate method

--- src/index.spec.js
+++ src/index.spec.js
@@ -5,16 +5,20 @@ describe( "lux-i18n - getFormattedMessage", () => {
 	it( "should pass the args straight through to the store accessor method", () => {
 		const msgStub = sinon.stub();
 		const numStub = sinon.stub();
+		const dateStub = sinon.stub();
 		const instance = proxyquire( "./", {
 			"./intl.store": {
 				getFormattedMessage: msgStub,
-				getFormattedNumber: numStub
+				getFormattedNumber: numStub,
+				getFormattedDate: dateStub
 			},
 			"./intlWrapper": {}
 		} );
 		instance.getFormattedMessage( "fake.key", { datums: true }, "O NOES!" );
 		instance.getFormattedNumber( 20, { style: "percent" } );
+		instance.getFormattedDate( new Date( Date.UTC( 2020, 12, 31, 0, 0, 0 ) ), { weekday: "long", year: "numeric", month: "short", day: "numeric" } );
 		msgStub.should.be.calledOnce.and.calledWith( "fake.key", { datums: true }, "O NOES!" );
 		numStub.should.be.calledOnce.and.calledWith( 20, { style: "percent" } );
+		dateStub.should.be.calledOnce.and.calledWith( new Date( Date.UTC( 2020, 12, 31, 0, 0, 0 ) ), { weekday: "long", year: "numeric", month: "short", day: "numeric" } );
 	} );
 } );
